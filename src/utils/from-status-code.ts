68fd024c3bbd192baa2cdf30fe7313901af2f405
Sylvain Estevez <sylvain@luxe.com>
2020-10-27 11:38:25 +0100

    :sparkles: Support for TS 4

--- src/utils/from-status-code.ts
+++ src/utils/from-status-code.ts
@@ -1,6 +1,6 @@
-import { StatusCode, isErrorStatusCode } from '@bluejay/status-code';
-import { RestError } from '../classes/rest-error';
+import { isErrorStatusCode, StatusCode } from '@bluejay/status-code';
 import { TConstructible } from '@bluejay/utils';
+import { RestError } from '../classes/rest-error';
 import { map } from '../misc/errors-map';
 import { TRestErrorCode } from '../types/rest-error-code';
 import { TRestErrorMeta } from '../types/rest-error-meta';
@@ -12,3 +12,5 @@ export function fromStatusCode(statusCode: StatusCode, message: string, meta?: T
 
   throw new Error(`${statusCode} is not a valid error code.`);
 }
+
+
